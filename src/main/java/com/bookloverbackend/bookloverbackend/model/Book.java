e1a6766bf687c592d636ac9e506b87671703cc97
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2022-11-22 14:36:56 +0200

    created database

---
+++ src/main/java/com/bookloverbackend/bookloverbackend/model/Book.java
@@ -0,0 +1,30 @@
+package com.bookloverbackend.bookloverbackend.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookId;
+
+    @NotNull
+    @Column(unique=true)
+    private String title;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private String category;
+}
