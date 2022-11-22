52607bd24a6d70d49a70f5932834799b8d56ca7c
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2022-11-22 19:11:38 +0200

    added bookDTO

--- src/main/java/com/bookloverbackend/bookloverbackend/security/service/BookService.java
+++ src/main/java/com/bookloverbackend/bookloverbackend/security/service/BookService.java
@@ -1,16 +1,20 @@
-package com.bookloverbackend.bookloverbackend.service;
+package com.bookloverbackend.bookloverbackend.security.service;
 
+import com.bookloverbackend.bookloverbackend.dto.BookDTO;
+import com.bookloverbackend.bookloverbackend.mapper.BookMapper;
 import com.bookloverbackend.bookloverbackend.model.Book;
 import com.bookloverbackend.bookloverbackend.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     public Book addBook(Book book){
         return bookRepository.save(book);
@@ -20,7 +24,9 @@ public class BookService {
         return bookRepository.findByBookId(bookId);
     }
 
-    public List<Book> getAllBooks(){
-        return bookRepository.findAll();
+    public List<BookDTO> getAllBooks(){
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toBookDTO)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/bookloverbackend/bookloverbackend/security/service/ReviewService.java
@@ -0,0 +1,9 @@
+package com.bookloverbackend.bookloverbackend.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+}
