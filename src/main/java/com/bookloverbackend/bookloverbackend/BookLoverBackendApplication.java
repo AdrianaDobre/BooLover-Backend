4a791738bebbd7e5cc68f84ca3cbfb066f554611
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2022-11-22 14:29:20 +0200

    created project

---
+++ src/main/java/com/bookloverbackend/bookloverbackend/BookLoverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookloverbackend.bookloverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoverBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLoverBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookloverbackend/bookloverbackend/BookLoverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookloverbackend.bookloverbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoverBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
