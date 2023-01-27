2ce14e4f94ee41258b8544f86d4089168d1ff9d7
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2023-01-27 20:43:15 +0200

    added improvements to BOOKDTO

--- src/main/java/com/bookloverbackend/bookloverbackend/mapper/BookMapper.java
+++ src/main/java/com/bookloverbackend/bookloverbackend/mapper/BookMapper.java
@@ -3,6 +3,7 @@ package com.bookloverbackend.bookloverbackend.mapper;
 import com.bookloverbackend.bookloverbackend.dto.BookDTO;
 import com.bookloverbackend.bookloverbackend.dto.ReviewDTO;
 import com.bookloverbackend.bookloverbackend.model.Book;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 import java.util.List;
